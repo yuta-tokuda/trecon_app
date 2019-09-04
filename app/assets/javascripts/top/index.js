eef1626dd50e0c53f1a1c8e06fb1bce2012e3d13
Tomoya Tanase <t.tanase@trecon.co.jp>
2019-09-04 10:44:13 +0900

    スクロール.4

--- app/assets/javascripts/top/index.js
+++ app/assets/javascripts/top/index.js
@@ -1,13 +1,11 @@
 $(function() {
-  return $(".page").infinitescroll({
+  $("#top .set_page").infinitescroll({
     loading: {
       img: "http://www.mytreedb.com/uploads/mytreedb/loader/ajax_loader_blue_48.gif",
       msgText: "ロード中..."
     },
-    navSelector: "nav.pagination",
-    nextSelector: "nav.pagination a[rel=next]",
+    navSelector: ".pagination",
+    nextSelector: ".pagination a[rel=next]",
     itemSelector: "#top tr.set_page"
   });
 });
-
-//= require infinite-scroll.pkgd
