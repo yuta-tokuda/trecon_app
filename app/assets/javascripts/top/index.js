1d52800fe7139ec0c947cc9e41218cacbb62a417
Tomoya Tanase <t.tanase@trecon.co.jp>
2019-09-05 16:27:00 +0900

    #41 無限scroll

--- app/assets/javascripts/top/index.js
+++ app/assets/javascripts/top/index.js
@@ -1,11 +1,12 @@
 $(function() {
-  $("#top .set_page").infinitescroll({
+  $("#set_page").infinitescroll({
     loading: {
       img: "http://www.mytreedb.com/uploads/mytreedb/loader/ajax_loader_blue_48.gif",
       msgText: "ロード中..."
     },
     navSelector: ".pagination",
     nextSelector: ".pagination a[rel=next]",
-    itemSelector: "#top tr.set_page"
+    itemSelector: "#set_page",
+    finishedMsg: "もうないよ"
   });
 });
