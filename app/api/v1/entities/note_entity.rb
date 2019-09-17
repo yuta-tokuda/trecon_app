7e11086f602a23dda8e995cbb63a35fce45fc127
shichida <m.shichida@trecon.co.jp>
2019-09-17 13:42:33 +0900

    ノート一覧取得API 修正

--- app/api/v1/entities/note_entity.rb
+++ app/api/v1/entities/note_entity.rb
@@ -2,10 +2,12 @@ module V1
   module Entities
     class NoteEntity < Grape::Entity
       expose :id
+      expose :favorite do |note, options|
+        note.favorite_note?(options[:current_user].id)
+      end
       expose :title
       expose :content
-      expose :created_by_user_id
-      expose :created_by_user_name do |note|
+      expose :created_by_user do |note|
         note.created_by_user.full_name
       end
       expose :created_at do |note|
@@ -16,21 +18,6 @@ module V1
       end
       expose :public_flag
       expose :tag_list
-
-      class NoteSearchEntity < Grape::Entity
-        expose :id
-        expose :favorite do |note, options|
-          note.favorite_note?(options[:current_user].id)
-        end
-        expose :title
-        expose :tag_list
-        expose :created_by_user_name do |note|
-          note.created_by_user.full_name
-        end
-        expose :created_at do |note|
-          note.created_at.strftime('%Y/%m/%d %H:%M')
-        end
-      end
     end
   end
 end
--- app/api/v1/notes.rb
+++ app/api/v1/notes.rb
@@ -14,7 +14,7 @@ module V1
         optional :title,              type: String
         optional :content,            type: String
         optional :tag_name,           type: String
-        optional :created_by_user,    type: Integer
+        optional :created_by_user_id, type: Integer
         optional :my_note_flag,       type: Boolean
         optional :favorite_note_flag, type: Boolean
       end
@@ -35,14 +35,15 @@ module V1
       end
 
       get '/' do
-        notes = Note.current_viewable_notes(current_user.id)
-        notes = notes.my_notes(current_user.id) if ActiveRecord::Type::Boolean.new.cast(params[:my_note_flag])
-        notes = notes.where(id: current_user.favorite_notes.map(&:note_id)) if ActiveRecord::Type::Boolean.new.cast(params[:favorite_note_flag])
+        notes = Note.current_viewable_notes(current_user.id).includes(:taggings, :created_by_user)
+        notes = notes.my_notes(current_user.id) if params[:my_note_flag]
+        notes = notes.where(id: current_user.favorite_notes.map(&:note_id)) if params[:favorite_note_flag]
+        notes = notes.where(created_by_user_id: params[:created_by_user_id]) if params[:created_by_user_id].present?
         notes = notes.tagged_with(params[:tag_name].to_s) if params[:tag_name].present?
         notes = notes.where(['title LIKE ?', "%#{ params[:title] }%"]) if params[:title].present?
         notes = notes.where(['content LIKE ?', "%#{ params[:content] }%"]) if params[:content].present?
 
-        present notes, with: Entities::NoteEntity::NoteSearchEntity, current_user: current_user
+        present notes, with: Entities::NoteEntity, current_user: current_user
       end
 
       desc 'POST /api/v1/notes/new'
@@ -61,7 +62,7 @@ module V1
 
         get '/' do
           note = Note.find(params[:id])
-          present note, with: Entities::NoteEntity
+          present note, with: Entities::NoteEntity, current_user: current_user
         end
 
         desc 'PATCH /api/v1/notes/:id'
